7f8fd2f1fad6cb121b97988ecfc2639395a5a8ef
Techpc22 <kayabala9@gmail.com>
2021-02-07 14:55:48 +0600

    Google Map

---
+++ app/src/main/java/com/example/googlemapapi/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.googlemapapi;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    GoogleMap mapAPI;
+    SupportMapFragment mapFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.mapAPI);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        if (mapAPI == null) {
+            mapAPI = googleMap;
+        }
+
+        LatLng sydney = new LatLng(-34, 151);
+        mapAPI.addMarker(new MarkerOptions()
+                .position(sydney)
+                .title("Marker in Sydney"));
+        mapAPI.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+}
+
+
---
+++ app/src/main/java/com/example/googlemapapi/onMapReadyCallback.java
@@ -0,0 +1,5 @@
+package com.example.googlemapapi;
+
+public interface onMapReadyCallback {
+
+}
